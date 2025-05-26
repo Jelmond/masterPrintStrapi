b86e195996531f9e57aedca33cfccaa13e7eb500
Jelmond <ant.soloviov@gmail.com>
2025-05-26 14:16:01 +0300

    feat: update category controller

--- src/api/category/controllers/category.ts
+++ src/api/category/controllers/category.ts
@@ -4,4 +4,26 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::category.category');
+export default factories.createCoreController('api::category.category', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.db.query('api::category.category').findOne({
+      where: { id },
+      populate: {
+        products: {
+          populate: ['images', 'category', 'tags']
+        }
+      }
+    });
+
+    console.log('entity', entity);
+
+    if (!entity) {
+      return ctx.notFound('Category not found');
+    }
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+    return this.transformResponse(sanitizedEntity);
+  }
+}));
