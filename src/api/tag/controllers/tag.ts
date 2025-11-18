dcc61b7aceeb51a885b6736df15b5c5aa920b75e
Jelmond <ant.soloviov@gmail.com>
2025-11-18 22:14:33 +0300

    feat: update populates

--- src/api/tag/controllers/tag.ts
+++ src/api/tag/controllers/tag.ts
@@ -6,71 +6,33 @@ import { factories } from '@strapi/strapi'
 
 export default factories.createCoreController('api::tag.tag', ({ strapi }) => ({
   async find(ctx) {
-    // Merge default populates for products with query populates
-    const defaultProductPopulate = {
-      batch: true,
-      designers: true,
-      polishes: true,
-      images: true,
-      categories: true,
-      tags: true,
+    // Always use the same populate structure
+    ctx.query.populate = {
+      products: {
+        batch: true,
+        designers: true,
+        polishes: true,
+        images: true,
+        categories: true,
+        tags: true,
+      },
     };
-    
-    // Parse existing populate from query or use default
-    const queryPopulate = ctx.query.populate 
-      ? (typeof ctx.query.populate === 'string' 
-          ? JSON.parse(ctx.query.populate) 
-          : ctx.query.populate)
-      : {};
-    
-    // If products are being populated, merge default product populates
-    if (queryPopulate.products || !ctx.query.populate) {
-      const mergedPopulate = {
-        ...queryPopulate,
-        products: {
-          ...defaultProductPopulate,
-          ...(typeof queryPopulate.products === 'object' ? queryPopulate.products : {}),
-        },
-      };
-      ctx.query.populate = mergedPopulate;
-    } else {
-      ctx.query.populate = queryPopulate;
-    }
     
     return await super.find(ctx);
   },
   
   async findOne(ctx) {
-    // Merge default populates for products with query populates
-    const defaultProductPopulate = {
-      batch: true,
-      designers: true,
-      polishes: true,
-      images: true,
-      categories: true,
-      tags: true,
+    // Always use the same populate structure
+    ctx.query.populate = {
+      products: {
+        batch: true,
+        designers: true,
+        polishes: true,
+        images: true,
+        categories: true,
+        tags: true,
+      },
     };
-    
-    // Parse existing populate from query or use default
-    const queryPopulate = ctx.query.populate 
-      ? (typeof ctx.query.populate === 'string' 
-          ? JSON.parse(ctx.query.populate) 
-          : ctx.query.populate)
-      : {};
-    
-    // If products are being populated, merge default product populates
-    if (queryPopulate.products || !ctx.query.populate) {
-      const mergedPopulate = {
-        ...queryPopulate,
-        products: {
-          ...defaultProductPopulate,
-          ...(typeof queryPopulate.products === 'object' ? queryPopulate.products : {}),
-        },
-      };
-      ctx.query.populate = mergedPopulate;
-    } else {
-      ctx.query.populate = queryPopulate;
-    }
     
     return await super.findOne(ctx);
   },
