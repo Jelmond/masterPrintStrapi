943162cc455c1c934051fc2077134aa97996b063
Jelmond <ant.soloviov@gmail.com>
2025-11-18 22:24:45 +0300

    feat: update populates

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -6,8 +6,8 @@ import { factories } from '@strapi/strapi'
 
 export default factories.createCoreController('api::product.product', ({ strapi }) => ({
   async find(ctx) {
-    // Merge default populates with query populates
-    const defaultPopulate = {
+    // Always use the same populate structure
+    ctx.query.populate = {
       batch: true,
       designers: true,
       polishes: true,
@@ -15,28 +15,13 @@ export default factories.createCoreController('api::product.product', ({ strapi 
       categories: true,
       tags: true,
     };
-    
-    // Parse existing populate from query or use default
-    const queryPopulate = ctx.query.populate 
-      ? (typeof ctx.query.populate === 'string' 
-          ? JSON.parse(ctx.query.populate) 
-          : ctx.query.populate)
-      : {};
-    
-    // Merge populates (query takes precedence for nested fields)
-    const mergedPopulate = {
-      ...defaultPopulate,
-      ...queryPopulate,
-    };
-    
-    ctx.query.populate = mergedPopulate;
     
     return await super.find(ctx);
   },
   
   async findOne(ctx) {
-    // Merge default populates with query populates
-    const defaultPopulate = {
+    // Always use the same populate structure
+    ctx.query.populate = {
       batch: true,
       designers: true,
       polishes: true,
@@ -44,21 +29,6 @@ export default factories.createCoreController('api::product.product', ({ strapi 
       categories: true,
       tags: true,
     };
-    
-    // Parse existing populate from query or use default
-    const queryPopulate = ctx.query.populate 
-      ? (typeof ctx.query.populate === 'string' 
-          ? JSON.parse(ctx.query.populate) 
-          : ctx.query.populate)
-      : {};
-    
-    // Merge populates (query takes precedence for nested fields)
-    const mergedPopulate = {
-      ...defaultPopulate,
-      ...queryPopulate,
-    };
-    
-    ctx.query.populate = mergedPopulate;
     
     return await super.findOne(ctx);
   },
